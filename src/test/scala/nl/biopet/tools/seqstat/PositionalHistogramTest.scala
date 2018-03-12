a6cf82ace2a9e53634907ff4b3b5d6b779b3cefb
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2018-03-12 08:38:22 +0100

    Auto format

--- src/test/scala/nl/biopet/tools/seqstat/PositionalHistogramTest.scala
+++ src/test/scala/nl/biopet/tools/seqstat/PositionalHistogramTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2014 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.seqstat
 
 import nl.biopet.test.BiopetTest
@@ -11,8 +32,14 @@ class PositionalHistogramTest extends BiopetTest {
     histogram.addRead("ATGC")
     histogram.totalBases shouldBe 4
     histogram.totalReads shouldBe 1
-    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0, 2 -> 0, 3 -> 0, 4 -> 1)
-    histogram.valueHistogram.countsMap shouldBe Map('A' -> 1, 'T' -> 1, 'G' -> 1, 'C' -> 1)
+    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0,
+                                                     2 -> 0,
+                                                     3 -> 0,
+                                                     4 -> 1)
+    histogram.valueHistogram.countsMap shouldBe Map('A' -> 1,
+                                                    'T' -> 1,
+                                                    'G' -> 1,
+                                                    'C' -> 1)
   }
 
   @Test
@@ -24,8 +51,15 @@ class PositionalHistogramTest extends BiopetTest {
     histogram += histogram2
     histogram.totalBases shouldBe 9
     histogram.totalReads shouldBe 2
-    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0, 2 -> 0, 3 -> 0, 4 -> 1, 5 -> 1)
-    histogram.valueHistogram.countsMap shouldBe Map('A' -> 2, 'T' -> 3, 'G' -> 2, 'C' -> 2)
+    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0,
+                                                     2 -> 0,
+                                                     3 -> 0,
+                                                     4 -> 1,
+                                                     5 -> 1)
+    histogram.valueHistogram.countsMap shouldBe Map('A' -> 2,
+                                                    'T' -> 3,
+                                                    'G' -> 2,
+                                                    'C' -> 2)
   }
 
   @Test
@@ -40,13 +74,19 @@ class PositionalHistogramTest extends BiopetTest {
   }
 
   @Test
-  def testJsonInput(): Unit ={
+  def testJsonInput(): Unit = {
     val jsonString = """{"G":[0,0,1],"A":[1],"C":[0,0,0,1],"T":[0,1]}"""
     val json = Json.parse(jsonString)
     val histogram = PositionalHistogram.fromJson(json)
     histogram.totalBases shouldBe 4
     histogram.totalReads shouldBe 1
-    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0, 2 -> 0, 3 -> 0, 4 -> 1)
-    histogram.valueHistogram.countsMap shouldBe Map('A' -> 1, 'T' -> 1, 'G' -> 1, 'C' -> 1)
+    histogram.lengthHistogram.countsMap shouldBe Map(1 -> 0,
+                                                     2 -> 0,
+                                                     3 -> 0,
+                                                     4 -> 1)
+    histogram.valueHistogram.countsMap shouldBe Map('A' -> 1,
+                                                    'T' -> 1,
+                                                    'G' -> 1,
+                                                    'C' -> 1)
   }
 }
